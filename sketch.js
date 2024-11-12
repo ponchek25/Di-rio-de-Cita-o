1d10c12cb79898d35fb67d688038135dac24af17
ponchek25 <125266736+ponchek25@users.noreply.github.com>
2024-11-12 20:47:46 -0300

    2

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
  
   function gerarQuote() {
     var quoteArray = [
-      '"A sociologia é um esporte de combate, a escola é violenta, a educação não é romântica como falam." Fabrízzia Santos',
+     '"A sociologia é um esporte de combate, a escola é violenta, a educação não é romântica como falam." Fabrízzia Santos',
       '“Os jovens brancos precisam se reconhecer como atores dessa violência.”   Fabrízzia Santos',
       '“Vida é dor, é sofrimento também” Beatriz Batista Silva',
       '“Não é todo dia que tenho orgulho, de ser preta e deficiente, as vezes queria ser branca e poder entrar em todos os lugares sem nenhuns problemas, essa estrutura nos leva a querer isso, por isso devemos ter orgulho para nos mantermos de pé.” Beatriz Batista Silva',
@@ -20,8 +20,8 @@ $(document).ready(function() {
     
    var randomQuote = quoteArray[Math.floor(Math.random() * quoteArray.length)];
     document.getElementById("quote").innerHTML = randomQuote;
-    var by = " %23ConselhosDaInês via https://codepen.io/daliannyvieira/pen/MewWBZ";
-    document.getElementById("tweet").href = "https://twitter.com/intent/tweet?&text=" + randomQuote + by;
+    var by = " %23indiretasdobem via https://codepen.io/itska/full/PzwyxZ/";
+    document.getElementById("tweet").href = "https://pos.uel.br/profsocio/" + randomQuote + by;
 }
  
 document.getElementById("quoteButton").onclick = gerarQuote;
